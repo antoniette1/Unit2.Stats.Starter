a16acff644a61b278eecd4aeabe0cbd7deacece9
antoniette1 <antoniette.j.moore@gmail.com>
2023-10-01 21:56:06 -0700

    homework complete

--- index.js
+++ index.js
@@ -37,61 +37,83 @@ console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length; 
 }
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the sum of the numbers
- */
-function getSum(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the mean of the numbers
- */
-function getMean(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the smallest of the numbers
- */
-function getMin(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the largest of the numbers
- */
-function getMax(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the range of the numbers (max - min)
- */
-function getRange(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the even numbers in the array
- */
-function getEvens(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the odd numbers in the array
- */
-function getOdds(numbers) {
-  // TODO
-}
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number} the sum of the numbers
+  */
+ function getSum(numbers) {
+   let sum = 0;
+   for(let number of numbers){
+     sum += number;
+   }
+   return sum;
+  }
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number} the mean of the numbers
+  */
+ function getMean(numbers) {
+  let sum = 0;
+  for(let i = 0; i < numbers.length; i+=1){
+   sum = sum + numbers[i]
+  }
+  sum = sum / numbers.length;
+  return sum
+ }
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number} the smallest of the numbers
+  */
+ function getMin(numbers) {
+ return Math.min.apply(Math, numbers)
+ }
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number} the largest of the numbers
+  */
+ function getMax(numbers) {
+   return Math.max.apply(Math, numbers)
+ }
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number} the range of the numbers (max - min)
+  */
+ function getRange(numbers) {
+ return Math.max.apply(Math, numbers) - Math.min.apply(Math, numbers)
+ }
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number[]} the even numbers in the array
+  */
+ function getEvens(numbers) {
+ even = []
+ for (let number of numbers) {
+ 
+   if (number%2 ===0){
+   even.push(number);
+   }
+ }
+ return even
+ }
+ 
+ /**
+  * @param {number[]} numbers an array of integers
+  * @returns {number[]} the odd numbers in the array
+  */
+ function getOdds(numbers) {
+   odd = []
+   for (let number of numbers) {
+     if (number%2 ===1){
+     odd.push(number);
+     }
+   }
+   return odd
+   }
